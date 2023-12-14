4b03a1f77b0b7f7cbb883262bb7878c35f9ded75
Jan Waś <jan@was.net.pl>
2023-12-14 20:36:50 +0100

    Allow building the Docker image locally

--- docker-build.sh
+++ docker-build.sh
@@ -3,18 +3,20 @@
 set -euo pipefail
 set -x
 
+OPTS=()
 if [ -f release.properties ]; then
     VERSION=$(grep 'project.rel.pl.net.was\\:trino-openapi=' release.properties | cut -d'=' -f2)
+    OPTS+=(--platform linux/amd64,linux/arm64 --push)
 else
     VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+    OPTS+=(--load)
 fi
 TRINO_VERSION=$(mvn help:evaluate -Dexpression=dep.trino.version -q -DforceStdout)
 TAG=nineinchnick/trino-openapi:$VERSION
 
 docker buildx build \
-    --platform linux/amd64,linux/arm64 \
     -t "$TAG" \
     --build-arg TRINO_VERSION="$TRINO_VERSION" \
     --build-arg VERSION="$VERSION" \
-    --push \
+    "${OPTS[@]}" \
     .
