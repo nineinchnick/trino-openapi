fa3db29aea5daee33397208df0c55e4db76d3954
Jan Waś <jan@was.net.pl>
2023-12-16 16:54:47 +0100

    Test DataDog and CloudFlare spec parsing

--- src/test/java/pl/net/was/TestOpenApiSpec.java
+++ src/test/java/pl/net/was/TestOpenApiSpec.java
@@ -24,17 +24,34 @@ import org.junit.jupiter.api.Test;
 import java.net.URL;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 
 import static io.trino.spi.connector.SchemaTableName.schemaTableName;
 import static io.trino.spi.type.BigintType.BIGINT;
+import static io.trino.spi.type.BooleanType.BOOLEAN;
+import static io.trino.spi.type.DecimalType.createDecimalType;
+import static io.trino.spi.type.DoubleType.DOUBLE;
+import static io.trino.spi.type.IntegerType.INTEGER;
+import static io.trino.spi.type.TimestampType.TIMESTAMP_MILLIS;
 import static io.trino.spi.type.VarcharType.VARCHAR;
 import static java.util.Objects.requireNonNull;
 import static pl.net.was.OpenApiSpec.SCHEMA_NAME;
 
 class TestOpenApiSpec
 {
+    private final Schema<?> intSchema = newSchema("integer");
+    private final Schema<?> stringSchema = newSchema("string");
+    private final Schema<?> booleanSchema = newSchema("boolean");
+    private final Schema<?> arraySchema = newSchema("array");
+    private final Schema<?> objectSchema = newSchema("object");
+
+    private static Schema<?> newSchema(String type)
+    {
+        Schema<?> schema = new Schema<>();
+        schema.setType(type);
+        return schema;
+    }
+
     @Test
     public void getGithubTables()
     {
@@ -119,17 +136,9 @@ class TestOpenApiSpec
                             .build();
                 })
                 .toList();
-        Schema<?> intSchema = new Schema<>();
-        intSchema.setType("integer");
-        Schema<?> stringSchema = new Schema<>();
-        stringSchema.setType("string");
-        Schema<?> arraySchema = new Schema<>();
-        arraySchema.setType("array");
-        Schema<?> objectSchema = new Schema<>();
-        objectSchema.setType("object");
         RowType categoryType = RowType.from(List.of(
-                new RowType.Field(Optional.of("id"), BIGINT),
-                new RowType.Field(Optional.of("name"), VARCHAR)));
+                RowType.field("id", BIGINT),
+                RowType.field("name", VARCHAR)));
         ArrayType photosType = new ArrayType(VARCHAR);
         ArrayType tagsType = new ArrayType(categoryType);
         Assertions.assertThat(petColumns)
@@ -173,6 +182,247 @@ class TestOpenApiSpec
                                 .setType(BIGINT).setSourceType(intSchema)
                                 .setRequiresPredicate(Map.of(PathItem.HttpMethod.GET, "path", PathItem.HttpMethod.DELETE, "path"))
                                 .setIsNullable(true)
+                                .build());
+    }
+
+    @Test
+    public void getDatadogTables()
+    {
+        OpenApiSpec spec = loadSpec("datadog.yaml");
+        Map<String, List<OpenApiColumn>> tables = spec.getTables();
+
+        Set<String> expected = Set.of(
+                "api_v2_events",
+                "api_v2_team",
+                "api_v2_metrics",
+                "api_v2_query_timeseries");
+        Assertions.assertThat(tables.keySet()).containsAll(expected);
+        OpenApiTableHandle tableHandle = spec.getTableHandle(schemaTableName(SCHEMA_NAME, "api_v2_query_timeseries"));
+        Assertions.assertThat(tableHandle.getSelectPath()).isEqualTo("/api/v2/query/timeseries");
+        Assertions.assertThat(tableHandle.getInsertPath()).isEqualTo("/api/v2/query/timeseries");
+        Assertions.assertThat(tableHandle.getUpdatePath()).isEqualTo("/api/v2/query/timeseries");
+        Assertions.assertThat(tableHandle.getDeletePath()).isNull();
+        List<OpenApiColumn> columns = tables.get("api_v2_query_timeseries").stream()
+                .map(column -> {
+                    // compare only source types, so rebuild it without any other attribute
+                    Schema<?> sourceType = new Schema<>();
+                    sourceType.setType(column.getSourceType().getType());
+                    return OpenApiColumn.builderFrom(column)
+                            .setSourceType(sourceType)
+                            .build();
+                })
+                .toList();
+        RowType dataType = RowType.from(List.of(
+                RowType.field("attributes", RowType.from(List.of(
+                        RowType.field("series", new ArrayType(RowType.from(List.of(
+                                RowType.field("group_tags", new ArrayType(VARCHAR)),
+                                RowType.field("query_index", INTEGER),
+                                RowType.field("unit", new ArrayType(RowType.from(List.of(
+                                        RowType.field("family", VARCHAR),
+                                        RowType.field("name", VARCHAR),
+                                        RowType.field("plural", VARCHAR),
+                                        RowType.field("scale_factor", DOUBLE),
+                                        RowType.field("short_name", VARCHAR))))))))),
+                        RowType.field("times", new ArrayType(BIGINT)),
+                        RowType.field("values", new ArrayType(new ArrayType(DOUBLE)))))),
+                RowType.field("type", VARCHAR)));
+        RowType dataReqType = RowType.from(List.of(
+                RowType.field("attributes", RowType.from(List.of(
+                        RowType.field("formulas", new ArrayType(RowType.from(List.of(
+                                RowType.field("formula", VARCHAR),
+                                RowType.field("limit", RowType.from(List.of(
+                                        RowType.field("count", INTEGER),
+                                        RowType.field("order", VARCHAR)))))))),
+                        RowType.field("from", BIGINT),
+                        RowType.field("interval", BIGINT),
+                        RowType.field("queries", new ArrayType(RowType.from(List.of(
+                                RowType.field("data_source", VARCHAR),
+                                RowType.field("name", VARCHAR),
+                                RowType.field("query", VARCHAR))))),
+                        RowType.field("to", BIGINT)))),
+                RowType.field("type", VARCHAR)));
+        Assertions.assertThat(columns)
+                .containsExactly(
+                        OpenApiColumn.builder()
+                                .setName("data").setSourceName("data")
+                                .setType(dataType).setSourceType(objectSchema)
+                                .setIsNullable(true)
+                                .setComment("A message containing the response to a timeseries query.")
+                                .build(),
+                        OpenApiColumn.builder()
+                                .setName("__trino_row_id")
+                                .setType(VARCHAR).setSourceType(stringSchema)
+                                .build(),
+                        OpenApiColumn.builder()
+                                .setName("data_req").setSourceName("data")
+                                .setType(dataReqType).setSourceType(objectSchema)
+                                .setRequiresPredicate(Map.of(PathItem.HttpMethod.POST, "body"))
+                                .setComment("A single timeseries query to be executed.")
+                                .build(),
+                        OpenApiColumn.builder()
+                                .setName("errors").setSourceName("errors")
+                                .setType(VARCHAR).setSourceType(stringSchema)
+                                .setIsNullable(true)
+                                .setComment("The error generated by the request.")
+                                .build());
+    }
+
+    @Test
+    public void getCloudflareTables()
+    {
+        OpenApiSpec spec = loadSpec("cloudflare.json");
+        Map<String, List<OpenApiColumn>> tables = spec.getTables();
+
+        Set<String> expected = Set.of(
+                "accounts",
+                "certificates",
+                "user",
+                "zones");
+        Assertions.assertThat(tables.keySet()).containsAll(expected);
+        OpenApiTableHandle tableHandle = spec.getTableHandle(schemaTableName(SCHEMA_NAME, "zones"));
+        Assertions.assertThat(tableHandle.getSelectPath()).isEqualTo("/zones");
+        Assertions.assertThat(tableHandle.getInsertPath()).isEqualTo("/zones");
+        Assertions.assertThat(tableHandle.getUpdatePath()).isEqualTo("/zones");
+        Assertions.assertThat(tableHandle.getDeletePath()).isEqualTo("/zones/{identifier}");
+        List<OpenApiColumn> columns = tables.get("zones").stream()
+                .map(column -> {
+                    // compare only source types, so rebuild it without any other attribute
+                    Schema<?> sourceType = new Schema<>();
+                    sourceType.setType(column.getSourceType().getType());
+                    return OpenApiColumn.builderFrom(column)
+                            .setSourceType(sourceType)
+                            .build();
+                })
+                .toList();
+        RowType resultType = RowType.from(List.of(
+                RowType.field("account", RowType.from(List.of(
+                        RowType.field("id", VARCHAR),
+                        RowType.field("name", VARCHAR)))),
+                RowType.field("activated_on", TIMESTAMP_MILLIS),
+                RowType.field("created_on", TIMESTAMP_MILLIS),
+                RowType.field("development_mode", createDecimalType(18, 8)),
+                RowType.field("id", VARCHAR),
+                RowType.field("meta", RowType.from(List.of(
+                        RowType.field("cdn_only", BOOLEAN),
+                        RowType.field("custom_certificate_quota", INTEGER),
+                        RowType.field("dns_only", BOOLEAN),
+                        RowType.field("foundation_dns", BOOLEAN),
+                        RowType.field("page_rule_quota", INTEGER),
+                        RowType.field("phishing_detected", BOOLEAN),
+                        RowType.field("step", INTEGER)))),
+                RowType.field("modified_on", TIMESTAMP_MILLIS),
+                RowType.field("name", VARCHAR),
+                RowType.field("original_dnshost", VARCHAR),
+                RowType.field("original_name_servers", new ArrayType(VARCHAR)),
+                RowType.field("original_registrar", VARCHAR),
+                RowType.field("owner", RowType.from(List.of(
+                        RowType.field("id", VARCHAR),
+                        RowType.field("name", VARCHAR),
+                        RowType.field("type", VARCHAR)))),
+                RowType.field("vanity_name_servers", new ArrayType(VARCHAR))));
+        RowType result2Type = RowType.from(List.of(RowType.field("id", VARCHAR)));
+        RowType resultInfoType = RowType.from(List.of(
+                RowType.field("count", createDecimalType(18, 8)),
+                RowType.field("page", createDecimalType(18, 8)),
+                RowType.field("per_page", createDecimalType(18, 8)),
+                RowType.field("total_count", createDecimalType(18, 8))));
+        Assertions.assertThat(columns)
+                .containsExactly(
+                        // TODO missing all query params
+                        OpenApiColumn.builder()
+                                .setName("result").setSourceName("result")
+                                .setType(new ArrayType(resultType)).setSourceType(arraySchema)
+                                .setIsNullable(true)
+                                .build(),
+                        OpenApiColumn.builder()
+                                .setName("result_2").setSourceName("result")
+                                .setType(result2Type).setSourceType(objectSchema)
+                                .setIsNullable(true)
+                                .build(),
+                        OpenApiColumn.builder()
+                                .setName("result_3").setSourceName("result")
+                                .setType(resultType).setSourceType(objectSchema)
+                                .setIsNullable(true)
+                                .build(),
+                        OpenApiColumn.builder()
+                                .setName("identifier").setSourceName("identifier")
+                                .setType(VARCHAR).setSourceType(stringSchema)
+                                // TODO we've merged /zones and /zones/{identifier} and now we require identifier for /zones, which is wrong
+                                .setRequiresPredicate(Map.of(PathItem.HttpMethod.GET, "path", PathItem.HttpMethod.DELETE, "path", PathItem.HttpMethod.PATCH, "path"))
+                                .setIsNullable(true)
+                                .setComment("Identifier")
+                                .build(),
+                        OpenApiColumn.builder()
+                                .setName("paused").setSourceName("paused")
+                                .setType(BOOLEAN).setSourceType(booleanSchema)
+                                .setRequiresPredicate(Map.of(PathItem.HttpMethod.PATCH, "body"))
+                                .setIsNullable(true)
+                                .setComment("Indicates whether the zone is only using Cloudflare DNS services. A\n" +
+                                        "true value means the zone will not receive security or performance\n" +
+                                        "benefits.\n")
+                                .build(),
+                        OpenApiColumn.builder()
+                                .setName("success").setSourceName("success")
+                                .setType(BOOLEAN).setSourceType(booleanSchema)
+                                .setComment("Whether the API call was successful")
+                                .build(),
+                        OpenApiColumn.builder()
+                                .setName("vanity_name_servers").setSourceName("vanity_name_servers")
+                                .setType(new ArrayType(VARCHAR)).setSourceType(arraySchema)
+                                .setRequiresPredicate(Map.of(PathItem.HttpMethod.PATCH, "body"))
+                                .setIsNullable(true)
+                                .setComment("An array of domains used for custom name servers. This is only\n" +
+                                        "available for Business and Enterprise plans.")
+                                .build(),
+                        OpenApiColumn.builder()
+                                .setName("result_info").setSourceName("result_info")
+                                .setType(resultInfoType).setSourceType(objectSchema)
+                                .setIsNullable(true)
+                                .build(),
+                        OpenApiColumn.builder()
+                                .setName("name").setSourceName("name")
+                                .setType(VARCHAR).setSourceType(stringSchema)
+                                .setRequiresPredicate(Map.of(PathItem.HttpMethod.POST, "body"))
+                                .setComment("The domain name")
+                                .build(),
+                        OpenApiColumn.builder()
+                                .setName("messages").setSourceName("messages")
+                                .setType(new ArrayType(RowType.from(List.of(
+                                        RowType.field("code", INTEGER),
+                                        RowType.field("message", VARCHAR))))).setSourceType(arraySchema)
+                                .build(),
+                        OpenApiColumn.builder()
+                                .setName("__trino_row_id")
+                                .setType(VARCHAR).setSourceType(stringSchema)
+                                .build(),
+                        OpenApiColumn.builder()
+                                .setName("type").setSourceName("type")
+                                .setType(VARCHAR).setSourceType(stringSchema)
+                                .setRequiresPredicate(Map.of(PathItem.HttpMethod.POST, "body", PathItem.HttpMethod.PATCH, "body"))
+                                .setIsNullable(true)
+                                .setComment("A full zone implies that DNS is hosted with Cloudflare. A partial zone is \n" +
+                                        "typically a partner-hosted zone or a CNAME setup.\n")
+                                .build(),
+                        OpenApiColumn.builder()
+                                .setName("plan").setSourceName("plan")
+                                .setType(RowType.from(List.of(RowType.field("id", VARCHAR)))).setSourceType(objectSchema)
+                                .setRequiresPredicate(Map.of(PathItem.HttpMethod.PATCH, "body"))
+                                .setIsNullable(true)
+                                .setComment("(Deprecated) Please use the `/zones/{identifier}/subscription` API\n" +
+                                        "to update a zone's plan. Changing this value will create/cancel\n" +
+                                        "associated subscriptions. To view available plans for this zone,\n" +
+                                        "see Zone Plans.\n")
+                                .build(),
+                        OpenApiColumn.builder()
+                                .setName("account").setSourceName("account")
+                                .setType(RowType.from(List.of(RowType.field("id", VARCHAR)))).setSourceType(objectSchema)
+                                .setRequiresPredicate(Map.of(PathItem.HttpMethod.POST, "body"))
+                                .build(),
+                        OpenApiColumn.builder()
+                                .setName("errors").setSourceName("errors")
+                                .setType(new ArrayType(RowType.from(List.of(
+                                        RowType.field("code", INTEGER),
+                                        RowType.field("message", VARCHAR))))).setSourceType(arraySchema)
                                 .build());
     }
 
