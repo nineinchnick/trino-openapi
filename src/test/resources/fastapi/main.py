c6c70116d188d79717f4be8c60f32f831dd554f2
Aleksander Lech <me@aleksander-lech.com>
2024-12-26 13:19:37 +0400

    Added some testcases for IN queries

--- src/test/resources/fastapi/main.py
+++ src/test/resources/fastapi/main.py
@@ -1,13 +1,12 @@
 from datetime import date, datetime
-from typing import Union
 
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
 
-
 class Item(BaseModel):
+    id: str
     name: str
     description: str | None = None
     price: float
@@ -18,22 +17,21 @@ class Item(BaseModel):
     validUntil: date = None
     revisedAt: list[date] = []
 
+class ItemFilter(BaseModel):
+    item_ids: list[str] = []
 
 items = {
-    1: Item(name="Portal Gun", price=42.0, tags=["sci-fi"], revisedAt=["2007-10-10", "2022-12-08"]),
-    2: Item(name="Plumbus", price=32.0, validUntil="2999-01-01", properties={"feeble": "schleem"}),
+    1: Item(id="1", name="Portal Gun", price=42.0, tags=["sci-fi"], revisedAt=["2007-10-10", "2022-12-08"]),
+    2: Item(id="2", name="Plumbus", price=32.0, validUntil="2999-01-01", properties={"feeble": "schleem"}),
 }
-
 
 @app.get("/")
 def root():
     return {"Hello": "World"}
 
-
-@app.get("/search")
-def search_items(q: Union[str, None] = None) -> list[Item]:
-    return items.values()
-
+@app.post("/search")
+def search_items(q: ItemFilter) -> list[Item]:
+    return filter(lambda item: item.id in q.item_ids,items.values())
 
 @app.get("/items/{item_id}")
 def get_item(item_id: int) -> Item:
